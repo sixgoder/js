5c2c4816187fc3df1eb8a0150f6cd6c12b53a194
rice <81219723@qq.com>
2023-01-29 16:33:54 +0800

    update exe-11 回调方法和拆分js文件

---
+++ exe/tools.js
@@ -0,0 +1,36 @@
+// 获取元素样式
+function getStyle(obj, name) {
+	if (window.getComputedStyle) {
+		return getComputedStyle(obj, null)[name];
+	} else {
+		//兼容IE8
+		return obj.currentStyle[name];
+	}
+}
+
+var timer;
+
+// 移动函数
+function move(obj, arr, target, speed, callback) {
+	clearInterval(obj.timer);
+
+	var oldValue = parseInt(getStyle(obj, arr));
+	if (oldValue > target) {
+		speed = -speed;
+	}
+
+	obj.timer = setInterval(function() {
+		var newValue = oldValue + speed;
+		oldValue = newValue;
+
+		if ((speed > 0 && newValue >= target) || (speed < 0 && newValue <= target)) {
+			newValue = target;
+		}
+
+		obj.style[arr] = newValue + "px";
+		if (newValue == target) {
+			clearInterval(obj.timer);
+			callback && callback();
+		}
+	}, 30);
+}
